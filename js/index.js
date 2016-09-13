ee5a55212d2c337b0833b2e2dd51887ab20bcf8e
mtso <mtso@mail.com>
2016-09-13 16:17:53 -0700

    Add image rasterizing

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
   
-  var canvas = $('#myCanvas');
-  
-  
+  var canvas = document.getElementById('myCanvas');
+  paper.setup(canvas);
+
+  paper.view.viewSize = new paper.Size(600, 600);
+
+  var raster = new paper.Raster('cover');
+  raster.position = new paper.Point(200, 200);
+  raster.scale(0.5);
+
+
+  paper.view.draw();
+
 });
