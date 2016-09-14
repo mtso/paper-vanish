860fa62c5fccf7f86225999aa8eb105b64624082
mtso <mtso@mail.com>
2016-09-13 21:21:33 -0700

    Animate vanish

--- js/index.js
+++ js/index.js
@@ -1,36 +1,90 @@
+
+var vanish;
+var begin = false;
+var timeElapsed = 0;
+
+
 $( document ).ready(function() {
   
   var canvas = document.getElementById('myCanvas');
   paper.setup(canvas);
-
   paper.view.viewSize = new paper.Size(300, 300);
 
   var raster = new paper.Raster('cover');
-  raster.position = paper.view.center;// new paper.Point(150, 150);
-  // raster.scale(0.5);
+  raster.position = paper.view.center;
+  paper.view.draw();
 
 
-  const particles = 10;
-  var circles = new paper.Path.Circle();
+  const particles = 15;
+  const duration = 1.5;
 
-  for (var i = 0; i < particles; i++) {
-    var x = Math.random() * paper.view.size.width | 0;
-    var y = Math.random() * paper.view.size.height | 0;
+  var totalWidth = paper.view.size.width;
+  var circles = [];
 
-    var newCircle = new paper.Path.Circle(new paper.Point(x, y), 30);
-    circles = circles.unite(newCircle);
+
+  vanish = function() {
+
+    while (circles.length > 0) {
+      circles.pop();
+    }
+
+    for (var i = 0; i < particles; i++) {
+      var x = Math.random() * paper.view.size.width | 0;
+      var y = Math.random() * paper.view.size.height | 0;
+      var noise = Math.random() * (totalWidth / 10) | 0;
+      var newCircle = {
+        x: x,
+        y: y,
+        sizeOffset: noise
+      }
+      circles.push(newCircle);
+    }
+
+    begin = true;
+    timeElapsed = 0;
+
+    $('#myCanvas').css({ filter: 'brightness(2000%)' });
+    setTimeout(function() {
+      $('#myCanvas').css({ filter: 'brightness(100%)' });
+    }, 40);
   }
 
 
-  var viewRect = new paper.Path.Rectangle(new paper.Point(0, 0), paper.view.size);
-  var mask = viewRect.subtract(circles);
-  // mask.fillColor = 'white';
+  paper.view.onFrame = function(event) {
 
-  var group = new paper.Group({
-    children: [mask, raster],
-    clipped: true
-  });
+    if (!begin) { return; }
 
-  paper.view.draw();
+    timeElapsed += event.delta;
+    if (timeElapsed > duration) {
+      begin = false;
+
+      var viewRect = new paper.Path.Rectangle(new paper.Point(0, 0), paper.view.size);
+      var group = new paper.Group({
+        children: [viewRect, raster],
+        clipped: false
+      });
+
+      paper.view.draw();
+    }
+    else {
+      var compositeCircles = new paper.Path.Circle();
+      for (var circle of circles) {
+        var size = (timeElapsed / duration) * totalWidth + circle.sizeOffset;        
+        var circleToAdd = new paper.Path.Circle(new paper.Point(circle.x, circle.y), size);
+        compositeCircles = compositeCircles.unite(circleToAdd);
+      }
+      var viewRect = new paper.Path.Rectangle(new paper.Point(0, 0), paper.view.size);
+      var mask = viewRect.subtract(compositeCircles);
+      
+      var group = new paper.Group({
+        children: [mask, raster],
+        clipped: true
+      });
+
+      paper.view.draw();
+    }
+  }
+
 
 });
+
